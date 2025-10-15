0a98a07bc3f3d4186f0ab5de7c447dc6423a762c
Romcodewasunavailable <120272499+Romcodewasunavailable@users.noreply.github.com>
2025-10-15 17:52:59 +0200

    thatsa lotta files

---
+++ arduidrone-controller/scripts/controller_axis_edit.gd
@@ -0,0 +1,15 @@
+@tool
+class_name ControllerAxisEdit
+extends Control
+
+@export var controller_axis: Controller.Axis:
+	set(value):
+		controller_axis = value
+		axis_name_label.text = Controller.Axis.keys()[controller_axis].capitalize()
+
+@export var axis_name_label: Label
+@export var axis_value_line_edit: LineEdit
+
+
+func _on_axis_value_line_edit_text_submitted(new_text: String) -> void:
+	Controller.axis_state[controller_axis] = float(new_text)
--- arduidrone-controller/scripts/controller_control_panel.gd
+++ arduidrone-controller/scripts/controller_control_panel.gd
@@ -16,31 +16,3 @@ func _on_poll_rate_line_edit_text_submitted(new_text: String) -> void:
 
 func _on_send_udp_check_button_toggled(toggled_on: bool) -> void:
 	Controller.set_send_udp(toggled_on)
-
-
-func _on_pitch_angle_p_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_ANGLE_P] = float(new_text)
-
-
-func _on_pitch_angle_i_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_ANGLE_I] = float(new_text)
-
-
-func _on_pitch_angle_d_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_ANGLE_D] = float(new_text)
-
-
-func _on_pitch_rate_p_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_RATE_P] = float(new_text)
-
-
-func _on_pitch_rate_i_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_RATE_I] = float(new_text)
-
-
-func _on_pitch_rate_d_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.PITCH_RATE_D] = float(new_text)
-
-
-func _on_gyro_rate_alpha_line_edit_text_submitted(new_text):
-	Controller.axis_state[Controller.Axis.GYRO_RATE_ALPHA] = float(new_text)
